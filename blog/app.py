833dfb70bb78a7507969e66afd23b07ea9a3b562
AndreiGosovich <andrei_gosovich@mail.ru>
2023-02-24 18:19:19 +0300

    Устранение ошибок

--- blog/app.py
+++ blog/app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Hello web!"
+    return "Hello web!"
 
 
 @app.route("/user/")
 def read_user():
-	name = request.args.get("name")
-	surname = request.args.get("surname")
-	return f"Hello {name or '[no name]'} {surname or '[no surname]'}!"
+    name = request.args.get("name")
+    surname = request.args.get("surname")
+    return f"Hello {name or '[no name]'} {surname or '[no surname]'}!"
