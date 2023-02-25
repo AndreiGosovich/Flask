4db5a17cc7f0a70c3326d1568ed1db18d26b79b0
AndreiGosovich <andrei_gosovich@mail.ru>
2023-02-25 11:45:11 +0300

    Lesson 2

--- blog/app.py
+++ blog/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
-from flask import request
+from flask import Flask, render_template, request
+from blog.views.users import users_app
+from blog.views.articles import articles_app
 
 app = Flask(__name__)
+
+app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix="/articles")
 
 
 @app.route("/")
 def index():
-    return "Hello web!"
+    return render_template("index.html")
 
 
 @app.route("/user/")
